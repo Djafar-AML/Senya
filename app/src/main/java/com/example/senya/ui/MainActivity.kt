e0b7c715cffb616fd1c20cfeee6ffd4e7609aea0
Jeff <Jafaragha77@gmail.com>
2023-02-20 20:52:08 +0330

    - appBarConfiguration added

--- app/src/main/java/com/example/senya/ui/MainActivity.kt
+++ app/src/main/java/com/example/senya/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.senya.R
 import com.example.senya.data.Attraction
 import com.example.senya.databinding.ActivityMainBinding
@@ -15,6 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
         private set
+
+    private val appBarConfiguration by lazy { initAppBarConfiguration() }
+
 
     val attractionList: List<Attraction> by lazy {
         parseRawAttractions(rawAttractions)
@@ -30,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupNavHostFragment()
+        setupActionBar()
     }
 
     private fun setupNavHostFragment() {
@@ -38,11 +45,23 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
     }
 
+    private fun setupActionBar() {
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
     private fun readRawAttractions() =
         resources.openRawResource(R.raw.croatia).bufferedReader().use { it.readText() }
 
     private fun parseRawAttractions(rawAttractions: String): List<Attraction> {
         return MoshiParse().parseRawAttractions(rawAttractions)
+    }
+
+    private fun initAppBarConfiguration(): AppBarConfiguration {
+        return AppBarConfiguration(navController.graph)
     }
 
 }
